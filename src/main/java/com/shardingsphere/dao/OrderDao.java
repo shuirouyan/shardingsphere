0c6a528a6900d44ed83fe003f40bf2de3cb67dfa
chenkang@aicaihr.com <1141761757@qq.com>
2022-08-21 11:59:35 +0800

    update dao

--- src/main/java/com/shardingsphere/dao/OrderDao.java
+++ src/main/java/com/shardingsphere/dao/OrderDao.java
@@ -3,13 +3,14 @@ package com.shardingsphere.dao;
 import com.shardingsphere.entity.Order;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * @author kangchen
  * @date 2022/8/20 11:46
  */
 //@Repository
-public interface OrderDao extends JpaRepository<Order, Long> {
+public interface OrderDao extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
 
     Order findByOrderId(Long orderId);
 }
--- src/main/java/com/shardingsphere/dao/UserDao.java
+++ src/main/java/com/shardingsphere/dao/UserDao.java
@@ -2,13 +2,14 @@ package com.shardingsphere.dao;
 
 import com.shardingsphere.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * @author kangchen
  * @date 2022/8/20 11:43
  */
 //@Repository
-public interface UserDao extends JpaRepository<User, Long> {
+public interface UserDao extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     User findByUserId(Long userId);
 }
